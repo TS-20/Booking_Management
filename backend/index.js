cd0a700e373c1dca6a5ae2809b92b85bacb493f9
sakshi <sakshisng27@gmail.com>
2025-07-15 15:31:15 +0530

    small bug fixed

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,18 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const connectDB = require('./config/db');
-const bookingroute= require('./route/booking.route');
 const userRoute = require('./route/user.route');
+//City orute has been imported here.
+const cityRoutes =require('./route/cityRoutes');
 
 // Connect to MongoDB
-
+connectDB();
 const app = express()
 app.use(express.json());
-app.use('/bookings', bookingroute);
-app.use('/users', userRoute);
-connectDB();
+app.use('/user', userRoute);
+//  inclusion of city route
+app.use('/cities',cityRoutes);
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
